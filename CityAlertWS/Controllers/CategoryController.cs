e2e25a1436e97eaa91290294e773dbe97e8677b0
Oana Ticu <oticu@totalsoft.ro>
2015-07-20 13:33:07 +0300

    Category list from DB

--- CityAlertWS/Controllers/CategoryController.cs
+++ CityAlertWS/Controllers/CategoryController.cs
@@ -5,47 +5,27 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CityAlertWS.Queries;
 
 namespace CityAlertWS.Controllers
 {
     public class CategoryController : ApiController
     {
+        private readonly DictionaryQueries _dicQueries = new DictionaryQueries();
+
+
         public IEnumerable<Category> Get()
         {
             List<Category> res = new List<Category>();
 
-            //try
-            //{
-            //    using (SqlConnection conn = new SqlConnection(SQLDBConnectionString()))
-            //    {
-            //        conn.Open();
-
-            //        SqlCommand cmd = conn.CreateCommand();
-            //        cmd.CommandText = "select * from mob_categories";
-            //        cmd.CommandType = CommandType.Text;
-            //        SqlDataReader reader = cmd.ExecuteReader();
-
-            //        if (reader.HasRows)
-            //        {
-            //            while (reader.Read())
-            //            {
-            //                Category cat = new Category();
-            //                cat.Id = (Guid)reader["id"];
-            //                cat.Name = reader["name"].ToString();
-            //                cat.ParentId = reader["parentid"] == System.DBNull.Value ? null : (Guid?)reader["parentid"];
-            //                cat.Statuscode = (int)reader["statuscode"];
-            //                cat.IsParent = Convert.ToBoolean(reader["isparent"]);
-            //                res.Add(cat);
-            //            }
-            //        }
-            //        reader.Close();
-            //        conn.Close();
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    var t = ex.Message;
-            //}
+            try
+            {
+                res = _dicQueries.GetCategories();
+            }
+            catch (Exception ex)
+            {
+                //var t = ex.Message;
+            }
 
             return res;
         }
---
+++ CityAlertWS/Queries/DictionaryQueries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CityAlert.Domain.Models;
+using CityAlertWS.Models;
+
+namespace CityAlertWS.Queries
+{
+    public class DictionaryQueries
+    {
+        private readonly CityAlertContext _context;
+
+        public DictionaryQueries()
+        {
+            _context = new CityAlertContext();
+        }
+
+        public List<Category> GetCategories()
+        {
+            return _context.SysCategories.Select(c => new Category()
+            {
+                Id = c.SysCategoryId,
+                Name = c.Name,
+                ParentId = c.ParentCategoryId,
+                IsParent = !c.ParentCategoryId.HasValue
+            }).ToList();
+        } 
+    }
+}
